99be2623fcf8f2d1d42385adc5a9226eb9fd112c
Thomas Cheyney <webmaster@thomasc.co.uk>
2011-01-31 02:34:00 +0000

    DtoH Fix

--- Conversions/Algorithms.vb
+++ Conversions/Algorithms.vb
@@ -4,12 +4,12 @@ Module Module1
         Dim bin As String = ""
 
         Console.WriteLine("Enter a binary number: ") : bin = Console.ReadLine
-        Console.WriteLine("Binary to decimal: " & BtoD(bin))
+        Console.WriteLine("Binary to denery: " & BtoD(bin))
 
-        Console.WriteLine("Enter a decimal number: ") : bin = Console.ReadLine
-        Console.WriteLine("Decimal to binary: " & DtoB(Val(bin)))
+        Console.WriteLine("Enter a denery number: ") : bin = Console.ReadLine
+        Console.WriteLine("Denery to binary: " & DtoB(Val(bin)))
 
-        Console.WriteLine("Enter a deenery number: ") : bin = Console.ReadLine
+        Console.WriteLine("Enter a denery number: ") : bin = Console.ReadLine
         Console.WriteLine("Denery to hex: " & DtoH(bin)) ' i'm a comment =D
         Console.ReadLine()
     End Sub
@@ -50,20 +50,18 @@ Module Module1
         Dim total As String = ""
         Dim tmp As Integer = 1
         Dim count As Byte = Math.Floor(Math.Log(bin, 16)) + 1 'Find the first power of 16 less than the total
-        For i = count to 0
-            tmp = Math.Pow(16, count - 1)
-            count -= 1 'Decrease power
+        For i = count - 1 To 0 Step -1
+            tmp = Math.Pow(16, i)
 
             Dim into As Integer = Math.Floor(bin / tmp) 'How many times does the power go in?
-            Select Case into
-                Case Is < 10
-                    total = total & into 'Just use the number
-                Case Else
-                    total = total & Chr(55 + into) 'Use ascii 65 = A, 66 = B etc..
-            End Select
+            If into < 10 Then
+                total = total & into 'Just use the number
+            Else
+                total = total & Chr(55 + into) 'Use ascii 65 = A, 66 = B etc..
+            End If
             bin -= into * tmp 'Decrease number
 
-        Loop Until count = 0 'When we've tried all powers we're done
+        Next 'When we've tried all powers we're done
         Return total 'I like return, +Good Programing Pracitce+
     End Function
 
