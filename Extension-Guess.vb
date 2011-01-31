19ccb9656c7342a14e788f192b7b84d33af92d6f
Thomas Cheyney <webmaster@thomasc.co.uk>
2011-01-31 18:31:45 +0000

    Extension from e-mail

---
+++ Extension-Guess.vb
@@ -0,0 +1,17 @@
+Module Module1
+
+    Sub Main()
+        Dim word, guess As String
+        Console.WriteLine(The new word)  word = Console.ReadLine().ToUpper()
+        Do
+            Console.WriteLine(Your guess)  guess = Console.ReadLine().ToUpper()
+
+            If word = guess Then
+                Console.WriteLine(Correct)
+            Else
+                Console.WriteLine(Wrong)
+            End If
+        Loop Until guess = word Or guess = NO MORE
+    End Sub
+
+End Module
